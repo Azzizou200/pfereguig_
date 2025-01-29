7e7a416398f83215c5719e6b0aba6e286bd2eb7a
Azzeddine Boubekeur <azzizou@gmail.com>
2025-01-29 20:29:20 +0100

    Added welcome screen

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -8,84 +8,61 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import { LinearGradient } from "expo-linear-gradient";
+
 export default function LoginScreen() {
   return (
-    <View>
-      <View
-        style={{
-          marginTop: 0,
-        }}
-      >
-        <ImageBackground
-          source={require("./../../assets/images/bus4.png")}
-          style={{
-            width: -50,
-            height: 780,
-          }}
-        >
-          <Text style={styles.box}>Opath</Text>
-          <Image
-            style={styles.logo}
-            source={require("./../../assets/images/logo.png")}
-          />
-          <Text style={styles.subText}>your journey starts with us</Text>
-          <TouchableOpacity style={styles.button}>
-            <Text
-              style={{
-                textAlign: "center",
-              }}
-            >
-              Get strated
-            </Text>
-          </TouchableOpacity>
-        </ImageBackground>
+    <ImageBackground
+      style={styles.container}
+      source={require("../../assets/images/bg.jpg")}
+    >
+      <View style={styles.iconholder}>
+        <Image
+          source={require("../../assets/images/logo.png")}
+          style={styles.icon}
+        ></Image>
+        <Text style={{ color: "#fff", marginLeft: 20, fontSize: 48 }}>
+          OPath
+        </Text>
       </View>
-      <View
-        style={{ backgroundColor: "black", height: 600, width: 600 }}
-      ></View>
-    </View>
+      <Text style={{ color: "gray", marginTop: 20 }}>
+        Start your journey with us
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={{ color: "#000", TextAlign: "center" }}>Get Started</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  box: {
-    fontSize: 48,
-    width: 200,
-    height: 100,
-
-    position: "absolute",
-    top: 548,
-    left: 178,
-    color: "white",
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
   },
-  logo: {
+  icon: {
     width: 50,
     height: 50,
-
-    position: "absolute",
-    top: 560,
-    left: 110,
-    color: "white",
   },
-  subText: {
-    position: "absolute",
-    top: 630,
-    left: 110,
-    color: "grey",
-    fontSize: 16,
-    width: 300,
-    height: 100,
+  iconholder: {
+    marginTop: 200,
+    flexDirection: "row",
+    flex: 0,
+    width: "100%",
+    height: "70",
+    backgroundColor: "",
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
-    padding: 10,
-    backgroundColor: "white",
-    position: "absolute",
-    top: 700,
-    left: 150,
-    color: "grey",
-    fontSize: 16,
+    marginTop: 20,
     width: 130,
-    height: 45,
-    borderRadius: 99,
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
