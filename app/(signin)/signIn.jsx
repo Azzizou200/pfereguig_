f64f35b056c2123ee0feee5a27fb4bb5fe476049
Ku26-31 <alirayen311@gmail.com>
2025-02-04 18:55:05 +0100

    Revert "refactoring sign up page"
    This reverts commit aeffe8241776dc99acd02bc9dca4b62d6b663d6e.

--- app/(signin)/signIn.jsx
+++ app/(signin)/signIn.jsx
@@ -1,79 +1,22 @@
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { ImageBackground, TouchableOpacity } from "react-native";
 import React from "react";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function signIn() {
   return (
-    <View style={styles.container}>
+    <ImageBackground
+      style={styles.container}
+      source={require("../../assets/images/bg.jpg")}
+    >
       <Image
         style={styles.icon}
         source={require("../../assets/images/logo.png")}
       ></Image>
       <Text style={styles.herotitle}>Sign In!</Text>
       <View style={styles.card}>
-        <Text style={styles.label}>Username</Text>
-        <TextInput placeholder="Username" style={styles.input}></TextInput>
-        <Text style={styles.label}>Email</Text>
-        <TextInput placeholder="Email" style={styles.input}></TextInput>
-        <Text style={styles.label}>Password</Text>
-        <TextInput placeholder="Password" style={styles.input}></TextInput>
-        <Text style={styles.label}>Confirm Password</Text>
-        <TextInput
-          placeholder="Password"
-          onFocus={() => {}}
-          style={styles.input}
-        ></TextInput>
-
-        <TouchableOpacity
-          style={{
-            marginTop: 20,
-            height: 50,
-            backgroundColor: "#000",
-            borderRadius: 20,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ color: "#fff", TextAlign: "center" }}>Sign In</Text>
-        </TouchableOpacity>
-        <View style={styles.authentications}>
-          <TouchableOpacity
-            style={{
-              marginTop: 5,
-              height: 50,
-              backgroundColor: "#000",
-              borderRadius: 20,
-              justifyContent: "center",
-              alignItems: "center",
-              width: "50%",
-            }}
-          >
-            <Image
-              style={{ width: 30, height: 30 }}
-              source={require("../../assets/images/communication.png")}
-            ></Image>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              flexDirection: "row",
-              marginTop: 5,
-              height: 50,
-              backgroundColor: "#000",
-              borderRadius: 20,
-              justifyContent: "center",
-              alignItems: "center",
-              width: "50%",
-            }}
-          >
-            <Image
-              style={{ width: 30, height: 30 }}
-              source={require("../../assets/images/search.png")}
-            ></Image>
-          </TouchableOpacity>
-        </View>
+        <Text style={styles.text}>Hollo</Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
@@ -84,42 +27,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   icon: {
-    marginTop: 90,
+    marginTop: 120,
     width: 50,
     height: 50,
   },
   herotitle: {
     marginTop: 20,
-    color: "#000",
+    color: "#fff",
     fontSize: 48,
   },
   text: {
     fontSize: 20,
-    color: "#000",
+    color: "#fff",
   },
   card: {
     marginTop: 20,
     width: 300,
 
-    // borderWidth: 1,
-  },
-  label: {
-    marginTop: 2,
-    marginLeft: 5,
-    color: "#000",
-    fontSize: 16,
-  },
-  authentications: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: "gray",
-
-    padding: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+    borderRadius: 5,
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderTopColor: "rgba(255, 255, 255, 0.3)",
+    borderLeftColor: "rgba(255, 255, 255, 0.3)",
+    borderRightColor: "rgba(255, 255, 255, 0.1)",
+    borderBottomColor: "rgba(255, 255, 255, 0.1)",
   },
 });
